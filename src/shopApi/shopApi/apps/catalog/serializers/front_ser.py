e35d1c460bca6d53150c54bc9689386ede573bd7
ReZ0iro <rezomoon@gmail.com>
2025-12-13 14:42:07 +0330

    ezafeh kardane serializer ha baraye app catalog dar 2 bakhshe yeki back va yeki front

---
+++ src/shopApi/shopApi/apps/catalog/serializers/front_ser.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from shopApi.apps.catalog.models import Category
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta : 
+
+        model = Category
+
+        fields  ='__all__' #  inke ch feild hayeero mikhayeem baramoon bargardoone 
+         
