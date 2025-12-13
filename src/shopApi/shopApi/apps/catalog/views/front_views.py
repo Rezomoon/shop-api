d3651b8f812d1f054c8f1219783ca4b4f582e7d2
ReZ0iro <rezomoon@gmail.com>
2025-12-13 14:45:57 +0330

    Add new view for first API

---
+++ src/shopApi/shopApi/apps/catalog/views/front_views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+from shopApi.apps.catalog.models import Category
+from shopApi.apps.catalog.serializers.front_ser import CategorySerializer
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet) :
+   
+    queryset            = Category.objects.all
+    serializer_class    = CategorySerializer
+
+    def get_queryset(self):
+        return Category.objects.filter(is_public=True)
+    
+   
