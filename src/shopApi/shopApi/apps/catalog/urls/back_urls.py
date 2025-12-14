ec9b13a31c2ce43fd431297ab97b823ba1210843
ReZ0iro <rezomoon@gmail.com>
2025-12-14 19:33:24 +0330

    Create and complete routes and urls with routers

--- src/shopApi/shopApi/apps/catalog/urls/back_urls.py
+++ src/shopApi/shopApi/apps/catalog/urls/back_urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from rest_framework.routers import SimpleRouter
+from shopApi.apps.catalog.views.back_views import CategoryViewSet
+
+# # #
+router = SimpleRouter()
+router.register('categories' , CategoryViewSet ,basename="CategoryshopApi/apps/catalog/serializers/back_ser.py") # Baraye Viewset hayee k get hastand momkene k get_query set nadashte bashand pas banabar in bayad baseName ro ezafeh kard
+urlpatterns = [] + router.urls  
