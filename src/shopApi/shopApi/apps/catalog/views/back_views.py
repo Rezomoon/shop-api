0a564a9c700719a558d77df8a29563529239566f
ReZ0iro <rezomoon@gmail.com>
2025-12-14 19:36:58 +0330

    Create API For Createing and get and post Data and get By Id and retriev

---
+++ src/shopApi/shopApi/apps/catalog/views/back_views.py
@@ -0,0 +1,36 @@
+from rest_framework import viewsets
+from shopApi.apps.catalog.models import Category
+from shopApi.apps.catalog.serializers.back_ser import CreateCategoryNodeSerializer ,CategorytreeSerializer , CategoryNodeTreeSerializer
+# 
+class CategoryViewSet(viewsets.ModelViewSet) : 
+    queryset     = Category.objects.all()
+    serializer_class    = CreateCategoryNodeSerializer
+
+
+    def get_queryset(self):
+        if self.action == "list" : 
+            return Category.objects.filter(depth = 1)
+        else : 
+            return Category.objects.all()
+        
+        return super().get_queryset()
+    def get_serializer_class(self):
+        # In baraye ine k agar darkhaste get dashtim data haro ba serialzer mojaza neshan bedeh
+        if self.action == "list" :
+            return CategorytreeSerializer
+        elif self.action == "create" :
+            return CreateCategoryNodeSerializer
+        elif self.action == "retrieve" :
+            return CategoryNodeTreeSerializer
+
+
+# Dar vaghe ma bayad va mitonestim az match pattern estefadeh konim ama py v balaye 3.10 mikhad 
+        # match self.action :
+        #     case "list" :
+        #         return CategorytreeSerializer
+        #     case "create" :
+        #         return CreateCategoryNodeSerializer
+        #     case  _ ""
+            
+        
+        return super().get_serializer_class()
