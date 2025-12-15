bbe42c5d4583d6e2e7cb83cb01c50d9ecd65a961
ReZ0iro <rezomoon@gmail.com>
2025-12-15 06:15:35 +0330

    Show Data For Front/view/User Side In Tree Shape and Like graphQl

--- src/shopApi/shopApi/apps/catalog/serializers/front_ser.py
+++ src/shopApi/shopApi/apps/catalog/serializers/front_ser.py
@@ -6,3 +6,15 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
 
         fields  ='__all__' #  inke ch feild hayeero mikhayeem baramoon bargardoone 
+
+class FrontCategoryTreesSerializer(serializers.ModelSerializer) :
+    # Show Data In Tree GraphgQl
+    
+    children    = serializers.SerializerMethodField()
+
+    def get_children(self,obj):
+        return FrontCategoryTreesSerializer(obj.get_children() , many = True).data
+    
+    class Meta : 
+        model   = Category
+        fields  = ("title" , "describtion" ,"is_public" , "slug" ,"children" )
--- src/shopApi/shopApi/apps/catalog/views/front_views.py
+++ src/shopApi/shopApi/apps/catalog/views/front_views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
 from shopApi.apps.catalog.models import Category
-from shopApi.apps.catalog.serializers.front_ser import CategorySerializer
+from shopApi.apps.catalog.serializers.front_ser import CategorySerializer , FrontCategoryTreesSerializer
 class CategoryViewSet(viewsets.ReadOnlyModelViewSet) :
    
     queryset            = Category.objects.public()
-    serializer_class    = CategorySerializer
+    serializer_class    = FrontCategoryTreesSerializer
 
     # def get_queryset(self):
     #     return Category.objects.filter(is_public=True)
